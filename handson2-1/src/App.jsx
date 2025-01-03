2fc198f2f4162234d1291e2b75accf3168075e07
naitsircnat <cristiangerardchen@gmail.com>
2025-01-03 06:55:17 +0000

    Added shout and image components containing props

--- handson2-1/src/App.jsx
+++ handson2-1/src/App.jsx
@@ -1,13 +1,38 @@
 import React from "react";
 import bicycle from "./assets/bicycle.png";
+import food from "./assets/food.png";
+
 import "./index.css";
 
 function Image() {
   return <img className="bicycle" src={bicycle} alt="bicycle" />;
 }
 
+function Image2(props) {
+  return <img className="bicycle" src={props.image} alt="unknown" />;
+}
+
+function Shout(props) {
+  return <>{props.message.toUpperCase()}</>;
+}
+
+function SumOfTwo(props) {
+  return props.num1 + props.num2;
+}
+
 function App() {
-  return <Image />;
+  return (
+    <>
+      <div>
+        <Shout message="Hello" />
+      </div>
+      <div>
+        <SumOfTwo num1={3} num2={4} />
+      </div>
+      <Image />
+      <Image2 image={food} />
+    </>
+  );
 }
 
 export default App;
